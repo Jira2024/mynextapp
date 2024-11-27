9e62b427fec8c8baacab70950bebf9b50db96b2c
Jirar Malone <jirapas.y@gmail.com>
2024-11-27 16:03:27 +0700

    first commit

---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <div className="px-2 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <h1 className="my-6 text-4xl font-bold">Contact Page</h1>
+        </div>  
+    )
+}
